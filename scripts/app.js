7c02a043a1582ab480e968d22359a383b2903ebf
AnsonAMS <anson_tohata@hotmail.com>
2021-01-28 14:03:58 +0100

    Updating scripts

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,47 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 
 // add a new chat
 newChatForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const message = newChatForm.message.value.trim();
-    chatroom.addChat(message)
-    .then(() => newChatForm.requestFullscreen())
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
     .catch(err => console.log(err));
 });
 
-// class instances
-const chatUI = new ChatUi(chatList);
-const chatroom = new Chatroom('general', 'shaun');
+// update the username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // update name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset the form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMssg.innerText = '', 3000);
+});
 
-// get chats and render 
+// update the chat room
+rooms.addEventListener('click', e => {
+  if(e.target.tagName === 'BUTTON'){
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
+});
+
+// check local storage for name
+const username = localStorage.username ? localStorage.username : 'anon';
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('gaming', username);
+
+// get chats & render
 chatroom.getChats(data => chatUI.render(data));
--- scripts/chat.js
+++ scripts/chat.js
@@ -4,49 +4,49 @@
 // updating the room 
 
 class Chatroom {
-    constructor(room, username) {
-        this.room = room;
-        this.username = username;
-        this.chats = db.collection('chats');
-        this.unsub;
+    constructor(room, username){
+      this.room = room;
+      this.username = username;
+      this.chats = db.collection('chats');
+      this.unsub;
     }
-    async addChat(message) {
-        // format a chat object
-        const now = new Date();
-        const chat = {
-            message,
-            username: this.username,
-            room: this.room,
-            created_at: firebase.firestore.Timestamp.fromDate(now)
-        };
-        // save the chat document
-        const response = await this.chats.add(chat);
-        return response;
+    async addChat(message){
+      // format a chat object
+      const now = new Date();
+      const chat = {
+        message: message,
+        username: this.username,
+        room: this.room,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+      };
+      // save the chat document
+      const response = await this.chats.add(chat);
+      return response;
     }
     getChats(callback){
-        this.unsub = this.chats
-            .where('room', '==', this.room)
-            .orderBy('created_at')
-            .onSnapshot(snapshot => {
-            snapshot.docChanges().forEach(change => {
-                if(change.type === 'added'){
-                    // update the ui
-                    callback(change.doc.data());
-                }
-            });
-        });
+      this.unsub = this.chats
+        .where('room', '==', this.room)
+        .orderBy('created_at')
+        .onSnapshot(snapshot => {
+          snapshot.docChanges().forEach(change => {
+            if(change.type === 'added'){
+              callback(change.doc.data());
+            }
+          });
+      });
     }
-    updateName(username) {
-        this.username = username; 
+    updateName(username){
+      this.username = username;
+      localStorage.username = username;
     }
-    updateRoom(room) {
-        this.room = room;
-        console.log("room updated");
-        if(this.unsub) {
+    updateRoom(room){
+      this.room = room;
+      console.log('room updated');
+      if(this.unsub){
         this.unsub();
-        }
+      }
     }
-}
+  }
 
 
 
